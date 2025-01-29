a53d15c21b5eb8983ac4d47d8b991058cc4403af
dumitrita1 <d.gorgan@hks-agentur.de>
2025-01-29 11:28:38 +0100

    add: css for login page

--- src/app/components/Login/login.component.ts
+++ src/app/components/Login/login.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import {AuthService} from "../../service/auth.service";
-import {FormsModule} from "@angular/forms";
-import {NgIf} from "@angular/common";
+import { AuthService } from '../../service/auth.service';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  imports: [
-    FormsModule,
-    NgIf
-  ]
 })
 export class LoginComponent {
   username: string = '';
