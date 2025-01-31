1098c21f0fcf7d2269c57ecb0c84ecb6a42281ad
dumitrita1 <d.gorgan@hks-agentur.de>
2025-01-31 15:11:30 +0100

    add: new path for new employee page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {LoginComponent} from "./components/Login/login.component";
 import {authGuard} from "./service/auth.guard";
 import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
 import {EmployeeListComponent} from "./components/employee-list/employee-list.component";
+import {NewEmployeeComponent} from "./components/employee/new-employee/new-employee.component";
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -12,6 +13,9 @@ export const appRoutes: Routes = [
   },
   {
     path: 'employee-list', component: EmployeeListComponent,
+  },
+  {
+    path: 'new-employee', component: NewEmployeeComponent,
   }
   ]
 
